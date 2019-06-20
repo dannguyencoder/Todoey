696d3f9079078e297f12720aa568d7ff96dc1a51
Daniel Nguyen <danielnguyen@Daniels-MacBook-Pro.local>
2019-06-20 13:47:18 +0700

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Daniel Nguyen on 6/19/19.
+//  Copyright © 2019 Daniel Nguyen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
